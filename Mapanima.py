542fa687a7bc607c050d12425c542365b839271d
lmguerrerov <lmguerrerov@udistrital.edu.co>
2025-05-19 14:35:45 -0500

    Version 100
    100

--- Mapanima.py
+++ Mapanima.py
@@ -79,7 +79,7 @@ if not st.session_state["autenticado"]:
 
     st.markdown("""
     <div class='seccion texto-bienvenida'>
-        <p><strong>Mapanima</strong> Es un visor étnico desarrollado para la Unidad de Restitución de Tierras, que representa el alma y la memoria territorial de los pueblos indígenas y comunidades negras de Colombia.</p>
+        <p><strong>Mapanima</strong> es un visor étnico desarrollado para la Unidad de Restitución de Tierras, que representa el alma y la memoria territorial de los pueblos indígenas y comunidades negras de Colombia.</p>
         <p>Su propósito es facilitar el análisis espacial de los procesos de restitución con una plataforma ligera, interactiva y respetuosa del carácter sagrado de la tierra.</p>
     </div>
     """, unsafe_allow_html=True)
@@ -122,7 +122,6 @@ if not st.session_state["autenticado"]:
 
 
 import streamlit as st
-st.set_page_config(page_title="Mapanima - Geovisor Étnico", layout="wide")
 
 # --- Estilo visual: tipografía, fondo, banner, leyenda ---
 
@@ -232,6 +231,7 @@ def cargar_shapefile_zip(uploaded_zip):
 
 import requests
 from io import BytesIO
+st.set_page_config(page_title="Mapanima - Geovisor Étnico", layout="wide")
 st.set_page_config(page_title="Mapanima - Geovisor Étnico", layout="wide")
 
 # --- Convertir link corto de OneDrive a link de descarga directa ---
